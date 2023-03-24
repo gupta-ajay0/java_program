c5f9b71e9e212a62a4d25b54a70c0e4d88da6eaa
Ajay Gupta <128709917+gupta-ajay0@users.noreply.github.com>
2023-03-24 09:45:33 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,18 @@
+class pattern
+{
+	public static void main(String arg[])
+	{
+		for(int i=1,j=10;i<=10;i++,j--)
+		{
+			System.out.println("5 * " + i + "= " + i*5 +"  ; 6 * "+ j + "= " +j*6);
+		}
+		System.out.println(" " );
+
+
+		char alpha []={'A','B','C','D'};
+		for(int i=0,j=3;i<4;i++,j--)
+		{
+			System.out.println(alpha[i]+" "+alpha[j]+" "+alpha[i]);
+		}
+	}
+}
